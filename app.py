6d5b2c560810d8ce10f2c9413c3f9d5f33d1b5fe
ADITYA PRABHAT GUPTA <aditygupta2474@gmail.com>
2026-04-05 18:37:08 +0530

    required changes

--- app.py
+++ app.py
@@ -113,5 +113,5 @@ HealthBot:"""
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 7860))  
     app.run(host='0.0.0.0', port=port)
